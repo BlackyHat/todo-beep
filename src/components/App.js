2db88549becc329d4b0a6ec130b8c67bd79b5e0c
Oleksandr P <goodwin009@gmail.com>
2023-02-08 12:04:02 +0200

    some fix code splitting

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, lazy } from "react";
 import { useDispatch } from "react-redux";
 
 import { Routes, Route } from "react-router-dom";
-import { refreshUser } from "redux/auth/auth-operations";
-import { useAuth } from "hooks/useAuth";
+import { refreshUser } from "../redux/auth/auth-operations";
+import { useAuth } from "../hooks/useAuth";
 import { RestrictedRoute } from "./RestrictedRoute";
 import { PrivateRoute } from "./PrivateRoute";
 
 import Layout from "./Layout/Layout";
 import Loading from "./Loading/Loading";
 
-import HomePage from "pages/HomePage/HomePage";
-import Tasks from "pages/Tasks";
-import LoginPage from "pages/LoginPage/LogInPage";
-import Register from "pages/Register/Register";
-
-// const HomePage = lazy(() => import("pages/HomePage/HomePage"));
-// const Tasks = lazy(() => import("pages/Tasks"));
-// const LoginPage = lazy(() => import("pages/LoginPage/LoginPage"));
-// const Register = lazy(() => import("pages/Register/Register"));
+const HomePage = lazy(() => import("../pages/HomePage/HomePage"));
+const Tasks = lazy(() => import("../pages/Tasks"));
+const LoginPage = lazy(() => import("../pages/LoginPage/LoginPage"));
+const Register = lazy(() => import("../pages/Register/Register"));
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -33,7 +28,7 @@ export const App = () => {
   ) : (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />}></Route>
+        <Route index element={<HomePage />} />
         <Route
           path="/todos"
           element={<PrivateRoute component={Tasks} redirectTo="/login" />}
